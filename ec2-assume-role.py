3978a55f00c39a9838ae75411c5f59995d12a0bc
Kelvin Galabuzi Lukoda <kelvingalabuzi@gmail.com>
2021-08-06 11:02:52 +0300

    updated the assume-role script

--- ec2-assume-role.py
+++ ec2-assume-role.py
@@ -3,7 +3,7 @@ import boto3
 sts_client = boto3.client('sts')
 
 assumed_role_object=sts_client.assume_role(
-    RoleArn="arn:aws:iam::585584209241:user/ec2-testuser",
+    RoleArn="arn:aws:iam::XXXXXXXXXXXX:user/ec2-testuser",
     RoleSessionName="Session1"
 )
 
@@ -11,9 +11,9 @@ credentials=assumed_role_object['Credentials']
 
 ec2_resource=boto3.resource(
     'ec2',
-    aws_access_key_id=credentials['AKIAYQV4J5VMYSZXT6HO'],
-    aws_secret_access_key=credentials['rkbRJrGFvB+ZXwBJXTKWCiSESc/Ip0iaWWK4KqOi'],
-    aws_session_token=credentials['Insert-Session-Your-Token-Here'],
+    aws_access_key_id=credentials['Insert-key-id-here'],
+    aws_secret_access_key=credentials['Insert-secret-access-key-here'],
+    aws_session_token=credentials['Insert-session-token-here'],
 )
 
 for resource in ec2_resource.instances.all():
